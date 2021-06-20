06e3fcb960d8d0e76493fb55115582c9e11887f3
pj <976859652@qq.com>
2021-06-21 04:28:18 +0800

    import vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+ <!--  <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -80,7 +80,53 @@
         </a>
       </li>
     </ul>
-  </div>
+  </div> -->
+
+   <v-row align="center">
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn small>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small color="primary">Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small color="error">Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small disabled>Disabled</v-btn>
+      </div>
+    </v-col>
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn color="primary">Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn color="error">Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn disabled>Disabled</v-btn>
+      </div>
+    </v-col>
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn large>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="primary">Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="error">Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large disabled>Disabled</v-btn>
+      </div>
+    </v-col>
+  </v-row>
+
 </template>
 
 <script>
